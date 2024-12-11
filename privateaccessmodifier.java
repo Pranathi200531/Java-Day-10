b541e8da5a5afebaedf2bcbf74f7374d0bf45485
Pranathi200531 <jpranathi04@gmail.com>
2024-12-11 13:21:47 +0530

    Add files via upload

---
+++ privateaccessmodifier.java
@@ -0,0 +1,10 @@
+class privateaccessmodifier{
+    private static void s()
+    {
+        System.out.println("Hello ");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+}
