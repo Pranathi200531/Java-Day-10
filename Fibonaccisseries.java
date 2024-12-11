c2b76888ae94b0867558563b514be2294280d004
Pranathi200531 <jpranathi04@gmail.com>
2024-12-11 13:21:10 +0530

    Add files via upload

---
+++ Fibonaccisseries.java
@@ -0,0 +1,14 @@
+class Fibonaccisseries{
+    public static int fib(int n)
+    {
+        if(n==0||n==1)
+        {
+            return 1;
+        }
+          return fib(n-1)+fib(n-2);
+    }
+    public static void main(String args[]){
+        int res=fib(7);
+        System.out.println(res);
+    }
+}
