cf2e0649f0460ce8dbc68a8854c9b76a6ee8dda1
Jettipalli Pranathi <jpranathi04@gmail.com>
2025-06-03 20:22:10 +0530

    Add files via upload

---
+++ withoutusingvoid.java
@@ -0,0 +1,10 @@
+Class withoutusingvoid{
+public static int s()
+    {
+        return"5";
+    }
+    public static void main(String args[]){
+    int res=s();
+        System.out.println(res);
+    }
+}
